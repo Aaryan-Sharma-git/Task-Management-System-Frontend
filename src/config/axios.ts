34fa7e09100214d6c3dcd2670386c654a5653bdf
Aaryan Sharma <aaryanradhe8@gmail.com>
2025-12-31 17:10:12 +0530

    setup env variables

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000", // change if needed
+  baseURL: import.meta.env.VITE_API_URL, // change if needed
   withCredentials: true, // useful if you use cookies later
 });
 
